373c0824da7bc111c2a395b6c8d7e70cd2e7b792
stoneneedle <stoneneedle@gmail.com>
2022-02-10 14:02:09 -0500

    Reverted to grid row in index file. Added addGiphyData() function that allows appending previewed GIFs to a new panel. Renamed getGiphyData to getGiphyPreview

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
 // Define variables for accessing Giphy's API & select DOM elements
 const base_url = "https://api.giphy.com/v1/gifs/search";
 const api_key = "OLKuCkrrmFhhJxSa9yLH2FzJvXJOuKQZ";
-// const gifSearch = document.getElementById('gifSearch');
-// let query = gifSearch.value;
 let limit = 5;
 
 const giphySearchResults = document.querySelector('#giphySearchResults');
 const debugPane = document.querySelector('#debugPane');
+gifSearch.addEventListener('keypress', getGiphyPreview);
 
-// debugPane.textContent = fetch_url;
-
-gifSearch.addEventListener('keypress', getGiphyData);
-
-function getGiphyData() {
+// Preview Giphy data in giphySearchResults pane prior to clicking
+function getGiphyPreview() {
   giphySearchResults.innerHTML = "";
   const gifSearch = document.getElementById('gifSearch');
   let query = encodeURI(gifSearch.value);
   let fetch_url = `${base_url}?q=${query}&api_key=${api_key}&limit=${limit}`;
-  //debugPane.textContent += fetch_url;
-  // console.log("Key pressed.");
+
   fetch(fetch_url)
     .then(function(response) {
       return response.json();
     }).then(function(data) {
-      console.log(data); // Debug
+      // console.log(data); // Debug
       data.data.forEach(function(value) {
         let gifDiv = document.createElement('div');
         gifDiv.setAttribute("class", "p-1");
@@ -35,6 +30,42 @@ function getGiphyData() {
         giphySearchResults.appendChild(gifDiv);
       })
     });
-    // .then(data => console.log(data));
 }
 
+const giphySearchBtn = document.querySelector('#giphySearchBtn');
+giphySearchBtn.addEventListener('click', addGiphyData);
+
+// Add Giphy data from typed query to a panel below
+function addGiphyData(e) {
+  const gifSearch = document.getElementById('gifSearch');
+  let query = encodeURI(gifSearch.value);
+  let fetch_url = `${base_url}?q=${query}&api_key=${api_key}&limit=${limit}`;
+
+  fetch(fetch_url)
+  .then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    // console.log(data); // Debug
+    let divRow = document.querySelector('.row');
+    let newCol = document.createElement('div');
+    newCol.setAttribute("class", "col-12 col-sm-6 p-3");
+    let newPanel = document.createElement('div');
+    newPanel.setAttribute("class", "p-3 bg-secondary rounded");
+
+    data.data.forEach(function(value) {
+      let gifDiv = document.createElement('div');
+      gifDiv.setAttribute("class", "p-1");
+      let gifImg = document.createElement('img');
+      gifImg.setAttribute("src", value.images.original.url);
+      gifImg.setAttribute("class", "img-fluid img-thumbnail");
+      gifImg.style.maxWidth = "100px";
+      gifDiv.appendChild(gifImg);
+      newPanel.appendChild(gifDiv);
+    });
+
+    newCol.appendChild(newPanel);
+    divRow.appendChild(newCol);
+
+    e.preventDefault();
+  });
+}
