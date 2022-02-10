d81f52ce2bec2fb816aebb3b55f718a46e058228
stoneneedle <stoneneedle@gmail.com>
2022-02-10 12:41:53 -0500

    Added basic image creation functionality, fixed the JS function to properly render images after each keypress, and added flex divs to the left input fields.

--- js/main-procyon.js
+++ js/main-procyon.js
@@ -1,23 +1,22 @@
 // Define variables for accessing Giphy's API & add to DOM
 const base_url = "https://api.giphy.com/v1/gifs/search";
 const api_key = "OLKuCkrrmFhhJxSa9yLH2FzJvXJOuKQZ";
-// const gifSearch = document.getElementById('gifSearch');
-// let query = gifSearch.value;
+const gifSearch = document.getElementById('gifSearch');
+let query = gifSearch.value;
 let limit = 5;
+
+let fetch_url = `${base_url}?q=${query}&api_key=${api_key}&limit=${limit}`;
 
 const giphySearchResults = document.querySelector('#giphySearchResults');
 const debugPane = document.querySelector('#debugPane');
 
-// debugPane.textContent = fetch_url;
+debugPane.textContent = fetch_url;
 
 gifSearch.addEventListener('keypress', getGiphyData);
 
 function getGiphyData() {
-  giphySearchResults.innerHTML = "";
-  const gifSearch = document.getElementById('gifSearch');
-  let query = gifSearch.value;
-  let fetch_url = `${base_url}?q=${query}&api_key=${api_key}&limit=${limit}`;
-  //debugPane.textContent += fetch_url;
+  query = gifSearch.value;
+  debugPane.textContent += fetch_url;
   // console.log("Key pressed.");
   fetch(fetch_url)
     .then(function(response) {
@@ -26,12 +25,14 @@ function getGiphyData() {
       console.log(data); // Debug
       data.data.forEach(function(value) {
         let gifDiv = document.createElement('div');
-        let gifImg = document.createElement('img');
-        gifImg.setAttribute("src", value.images.original.url);
-        gifDiv.appendChild(gifImg);
+        gifDiv.textContent = "Test";
         giphySearchResults.appendChild(gifDiv);
       })
-    });
+    }
+      
+
+
+    );
     // .then(data => console.log(data));
 }
 
