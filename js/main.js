57e995e9dbb2c1b3be3a7488240000aa6ce66634
stoneneedle <stoneneedle@gmail.com>
2022-04-11 15:30:08 -0400

    Previous multiple functions in main.js removed in favor of one simple API call function, which is updated to use template literals for HTML DOM changes, remove click and keypress event listeners in favor of a single submit event listener, eliminate the random Giphy API endpoint which was not working as expected for evaluator, preview feature was eliminated, and very basic input validation was implemented.

--- js/main.js
+++ js/main.js
@@ -1,123 +1,43 @@
 // Define variables for accessing Giphy's API & select DOM elements
 const base_url = "https://api.giphy.com/v1/gifs/search";
-const rand_url = "https://api.giphy.com/v1/gifs/random";
 const api_key = "OLKuCkrrmFhhJxSa9yLH2FzJvXJOuKQZ";
 let numResults = "5";
-let randResults = "5";
+const giphySearchForm = document.querySelector(".giphySearchForm-js");
 
-gifSearch.addEventListener('keypress', getGiphyPreview);
-randGifTag.addEventListener('keypress', randGiphyPreview);
-
-// Helper function to build div and image
-function createGiphyImgDiv(imgObj) {
-  // let gif_div = document.createElement('div');
-  // gif_div.setAttribute("class", "p-1");
-  // let gifImg = document.createElement('img');
-  // gifImg.setAttribute("src", imgObj.images.downsized.url);
-  // gifImg.setAttribute("class", "img-fluid img-thumbnail");
-  // gifImg.setAttribute("alt", imgObj.id)
-  // gifImg.style.maxWidth = "200px";
-  // gif_div.appendChild(gifImg);
-  // return gif_div;
-
-  // return `<div class="p-1">
-  // <img class="img-fluid img-thumbnail" src="${imgObj.downsized.url}" alt="${imgObj.id}" width="200" />
-  // </div>
-  // `;
-
-  return 'TEST';
-
-}
-
-// Preview Giphy data in giphySearchResults pane prior to clicking
-function getGiphyPreview() {
-  numResults = document.querySelector('#numResults').value;
-  giphySearchResults.innerHTML = "";
-  const gifSearch = document.getElementById('gifSearch');
-  let query = encodeURI(gifSearch.value);
-  let fetch_url = `${base_url}?q=${query}&api_key=${api_key}&limit=${numResults}`;
-
-  fetch(fetch_url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      data.data.forEach(function(value) {
-        let gifDiv = createGiphyImgDiv(value);
-        giphySearchResults.appendChild(gifDiv);
-      });
-    });
-}
-
-function randGiphyPreview() {
-  numResults = document.querySelector('#randNumResults').value;
-  giphyRandResults.innerHTML = "";
-  const randGifTag = document.getElementById('randGifTag');
-  let tag = encodeURI(randGifTag.value);
-  fetch_url = `${rand_url}?tag=${tag}&api_key=${api_key}&limit=${numResults}`;
-
-  fetch(fetch_url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      let gifDiv = createGiphyImgDiv(data.data);
-      giphyRandResults.appendChild(gifDiv);
-      });
-}
-
-const giphySearchBtn = document.querySelector('#giphySearchBtn');
-giphySearchBtn.addEventListener('click', addGiphyData);
+giphySearchForm.addEventListener("submit", addGiphyData);
 
 // Add Giphy data from typed query to a panel below
-function addGiphyData() {
-  numResults = document.querySelector('#numResults').value;
-  const gifSearch = document.getElementById('gifSearch');
-  let query = encodeURI(gifSearch.value);
-  let fetch_url = `${base_url}?q=${query}&api_key=${api_key}&limit=${numResults}`;
+function addGiphyData(e) {
+  e.preventDefault();
 
-  fetch(fetch_url)
-  .then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    let divRow = document.querySelector('.row');
-    let newCol = document.createElement('div');
-    newCol.setAttribute("class", "col-12 col-sm-6 p-3");
-    let newPanel = document.createElement('div');
-    newPanel.setAttribute("class", "d-flex flex-wrap bg-primary rounded");
+  const gifSearch = encodeURI(document.querySelector(".gifSearch-js").value);
 
-    data.data.forEach(function(value) {
-      let gifDiv = createGiphyImgDiv(value);
-      newPanel.appendChild(gifDiv);
-    });
+  if (gifSearch != "") {
+    numResults = document.querySelector(".numResults-js").value;
+    let fetch_url = `${base_url}?q=${gifSearch}&api_key=${api_key}&limit=${numResults}`;
+    let imgsDisplayDiv = ``;
+    let giphySearchResults = document.querySelector(".giphySearchResults-js");
 
-    newCol.appendChild(newPanel);
-    divRow.appendChild(newCol);
-  });
-}
-
-const giphyRandBtn = document.querySelector('#giphyRandBtn');
-giphyRandBtn.addEventListener('click', addRandGifs);
-
-function addRandGifs() {
-  numResults = document.querySelector('#randNumResults').value;
-  const randGifTag = document.getElementById('randGifTag');
-  let tag = encodeURI(randGifTag.value);
-  fetch_url = `${rand_url}?q=${tag}&api_key=${api_key}`;
-
-  let divRow = document.querySelector('.row');
-  let newCol = document.createElement('div');
-  newCol.setAttribute("class", "col-12 col-sm-6 p-3");
-  let newPanel = document.createElement('div');
-  newPanel.setAttribute("class", "d-flex flex-wrap bg-warning rounded");
-
-  for (let i = 0; i < parseInt(numResults); i++) {
     fetch(fetch_url)
-    .then(function(response) {
-      return response.json();
-    }).then(function(data) {
-      let randImgDiv = createGiphyImgDiv(data.data);
-      newPanel.appendChild(randImgDiv);
-    });
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.data.length != 0) {
+          imgsDisplayDiv += `<div class="col-12 col-sm-12 p-3">
+          <div class="d-flex flex-wrap bg-primary rounded p-3">`;
+
+          data.data.forEach(function (imgObj) {
+            imgsDisplayDiv += `<div class="p-3">
+            <img class="img-fluid img-thumbnail" src="${imgObj.images.downsized.url}" alt="${imgObj.id}" width="200" />
+            </div>`;
+          });
+
+          imgsDisplayDiv += `</div>
+          </div>`;
+
+          giphySearchResults.innerHTML = imgsDisplayDiv;
+        }
+      });
   }
-  newCol.appendChild(newPanel);
-  divRow.appendChild(newCol);
 }
